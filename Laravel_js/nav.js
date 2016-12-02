6acd2d9017bcd0815bf27795d0a690ee1b10a0aa
zqy <1457962543@qq.com>
2016-12-02 21:01:22 +0800

    xp

---
+++ Laravel_js/nav.js
@@ -0,0 +1,13 @@
+// $(function  () {
+// 	var num = 0
+// 	$(".label").click(function  () {
+// 		if (num++ % 2 == 0) {
+// 			$(".nav_ul").slideDown("last");
+// 		}else{
+// 			$(".nav_ul").slideUp("last");
+// 			if (num == 2) {
+// 				num = 0;
+// 			};
+// 		}
+//    })
+// })	
